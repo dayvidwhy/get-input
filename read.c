49729ab87319ca6cd10a9a94c0e66f61e080f358
dayvidwhy <dayvidwhy@gmail.com>
2021-09-11 14:45:33 +1000

    Close file after reading it.

--- read.c
+++ read.c
@@ -6,7 +6,8 @@
 enum Errors {
     NORMAL = 0,
     USAGE = 1,
-    FILE_ERROR = 2
+    FILE_ERROR = 2,
+    CLOSE_ERROR = 3
 };
 
 /* pass a message to stderr */
@@ -34,6 +35,9 @@ int post_status(enum Errors error) {
         case FILE_ERROR:
             message = "File could not be read.";
             break;
+        case CLOSE_ERROR:
+            message = "File could not be closed.";
+            break;
         default:
             message = "Unknown error";
     }
@@ -47,12 +51,21 @@ int main(int argc, char** argv) {
         post_status(USAGE);
     }
 
+    /* open the file */
     FILE* file;
     if (!(file = fopen(argv[1], "r"))) {
         post_status(FILE_ERROR);
     }
 
+    /* read the line */
     char* line = get_line(file);
+
+    /* close our file */
+    if (fclose(file) != 0) {
+        post_status(CLOSE_ERROR);
+    }
+
+    /* post the result */
     print("Line was:");
     print(line);
 
